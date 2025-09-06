278bdba008f937cd26e102063e6810e78e1b0d9f
Aravind C Pradeep <acp.ca@outlook.in>
2025-09-06 18:52:23 +0530

    removed database

--- app.js
+++ app.js
@@ -5,41 +5,14 @@ const app = express();
 const port = 
 app.use(express.json());
 
-const url = 'mongodb+srv://aravindpradeepca_db_user:Tx78WpwmyMpFFCM9@aravindcp12.k670gfn.mongodb.net/';
-mongoose.connect(url,{useNewUrlParser:true});
-
-const con = mongoose.connection;
-
-con.on('open',()=>{
-    console.log('Connected');
-});
-
-const bookSchema = new mongoose.Schema({
-    title:String,
-    year:Number
-})
-
-const book = new mongoose.model('book',bookSchema);
-
-
 app.get('/',(req,res)=>{
-    findbook('2022').then(data=>{
-        res.json(data);
-    })
+    res.send('This is our response for this Get Request');
 })
 
 app.post('/',(req,res)=>{
-    var receivedbook = req.body;
-    var newBook = new book(receivedbook);
-    newBook.save();
     res.send('Saved');
 })
 
 app.listen(5000,()=>{
     console.log('server started..');
 });
-
-async function findbook(year){
-    var bookfind = await book.find({year:year}).exec();
-    return bookfind;
-}
