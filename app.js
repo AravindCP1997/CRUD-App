52bac9e7da922218a34713a200543c88d4619ad4
Aravind C Pradeep <acp.ca@outlook.in>
2025-09-11 13:55:19 +0530

    integration

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'dist')));
 
 const port = 3000;
 app.use(express.json());
@@ -12,11 +15,15 @@ const data = {
     age:28
 }
 
-app.get('/',(req,res)=>{
+app.get('/api',(req,res)=>{
     res.json(data);
 })
 
-app.post('/',(req,res)=>{
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'dist','index.html'))
+})
+
+app.post('/api',(req,res)=>{
     res.send('Saved');
 })
 
